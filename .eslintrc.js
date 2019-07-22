d7dd244ff5cbd742de591c8b7c6e6ca64a39bc5f
Veselin <veselin.lk.petrov@gmail.com>
2019-07-22 18:08:16 +0300

    switch from google to airbnb eslint rules and customize them

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,39 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-  },
-  'extends': [
-    'google',
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-  },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
-  },
-  'rules': {
-    'new-cap': 0,
-    'object-curly-spacing': 0,
-    'require-jsdoc': 0,
-  },
-  'globals': {
-    p5: true,
-  },
-};
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    globals: {
+        Atomics: 'readonly',
+        p5: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    rules: {
+        // 'brace-style': ['error', 'stroustrup'],
+        // 'camelcase': 0,
+        // 'default-case': 0,
+        'indent': ['error',
+            4,
+            { 'SwitchCase': 1 }
+        ],
+        // 'new-cap': 0,
+        // 'max-len': 0,
+        'new-cap': 0,
+        // 'no-bitwise': 0,
+        'no-console': 0,
+        // 'no-param-reassign': 0,
+        // 'no-underscore-dangle': 0,
+        'no-plusplus': 0,
+        // 'prefer-destructuring': 0,
+        // 'prefer-template': 0,
+        'semi': 0,
+        'semi-style': ['error', 'first'],
+    },
+}
